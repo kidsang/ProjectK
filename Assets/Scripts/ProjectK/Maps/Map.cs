85bd5b46d2b786849756396e8289f398f4549dfd
kidsang <963262214@qq.com>
2015-06-23 20:26:25 +0800

    新增了六边形地图

---
+++ Assets/Scripts/ProjectK/Maps/Map.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Assets.Scripts.ProjectK.Base;
+
+namespace Assets.Scripts.ProjectK.Maps
+{
+
+    /**
+     * 左上角为地图的起始点，其中x = 0，y = 0。
+     * 参见 http://www.redblobgames.com/grids/hexagons
+     */
+    public class Map : DisposableBehaviour
+    {
+        private GameObject mapRoot;
+        private ResourceLoader loader;
+
+        private Dictionary<int, MapCell> cells = new Dictionary<int, MapCell>();
+
+        internal void Init(ResourceLoader loader)
+        {
+            mapRoot = gameObject;
+            this.loader = loader;
+        }
+
+        protected override void OnDispose()
+        {
+            loader = null;
+            DestroyObject(mapRoot);
+
+            base.OnDispose();
+        }
+
+        public MapCell GetCell(short x, short y)
+        {
+            int key = MapCell.MakeKey(x, y);
+            MapCell cell = null;
+            cells.TryGetValue(key, out cell);
+            return cell;
+        }
+
+        public MapCell GetCellBySceneXY(float sceneX, float sceneY)
+        {
+            short x = (short)(sceneX * 2 / 3.0f / MapCell.Radius);
+            short y = (short)((MapCell.Sqrt3 * sceneY - sceneX) / 3.0f / MapCell.Radius);
+            return GetCell(x, y);
+        }
+
+        public MapCell GetCellBySceneXY(Vector3 scenePos)
+        {
+            return GetCellBySceneXY(scenePos.x, scenePos.y);
+        }
+
+
+        // ----------------
+        // for editor
+
+        public void AddCell(short x, short y)
+        {
+            GameObject cellObject = new GameObject();
+            cellObject.transform.parent = mapRoot.transform;
+            MapCell cell = cellObject.AddComponent<MapCell>();
+            cell.Init(loader, x, y);
+            cells.Add(cell.Key, cell);
+        }
+
+        public void ResizeMap(short countX, short countY)
+        {
+            Dictionary<int, MapCell> oldCells = cells;
+            cells = new Dictionary<int, MapCell>();
+
+            for (short j = 0; j < countY; ++j)
+            {
+                for (short i = 0; i < countX; ++i)
+                {
+                    short x = i;
+                    short y = (short)(j - i / 2);
+
+                    int key = MapCell.MakeKey(x, y);
+                    if (oldCells.ContainsKey(key))
+                    {
+                        MapCell cell = oldCells[key];
+                        oldCells.Remove(key);
+                        cells.Add(key, cell);
+                    }
+                    else
+                    {
+                        AddCell(x, y);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectK/Maps/MapCell.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Assets.Scripts.ProjectK.Base;
+
+namespace Assets.Scripts.ProjectK.Maps
+{
+    /**
+     * MapCell使用Axial坐标系，其中x + y + z = 0，因此只需要x和y的就可以确定一个MapCell。
+     * 参见 http://www.redblobgames.com/grids/hexagons
+     */
+    public class MapCell : DisposableBehaviour
+    {
+        public static float Radius = 1.0f;
+        public static float Sqrt3 = Mathf.Sqrt(3.0f);
+
+        private GameObject cellObject;
+        private ResourceLoader loader;
+
+        private short x;
+        private short y;
+        private short z;
+        private int key;
+
+        internal void Init(ResourceLoader loader, short x, short y)
+        {
+            cellObject = gameObject;
+            this.loader = loader;
+
+            this.x = x;
+            this.y = y;
+            this.z = (short)(-x - y);
+            this.key = MakeKey(x, y);
+
+            cellObject.transform.position = new Vector3(CenterX, CenterY);
+        }
+
+        protected override void OnDispose()
+        {
+            loader = null;
+            DestroyObject(cellObject);
+
+            base.OnDispose();
+        }
+
+        public short X
+        {
+            get { return x; }
+        }
+
+        public short Y
+        {
+            get { return y; }
+        }
+
+        public short Z
+        {
+            get { return z; }
+        }
+
+        public int Key
+        {
+            get { return key; }
+        }
+
+        public float CenterX
+        {
+            get { return Radius * 1.5f * x; }
+        }
+
+        public float CenterY
+        {
+            get { return Radius * Sqrt3 * (x + 0.5f * y); }
+        }
+
+        public static int MakeKey(short x, short y)
+        {
+            return (y << 16) | (ushort)x;
+        }
+    }
+}
